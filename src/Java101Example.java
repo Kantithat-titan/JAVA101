55f2886c6adc6bd15b51d7d173218e1d72e67c16
WaterMelon <kantithat.sut@gmail.com>
2020-03-29 15:49:35 +0700

    Refactor Simple Code

--- src/Java101Example.java
+++ src/Java101Example.java
@@ -5,8 +5,10 @@ public class Java101Example {
 
     public static void main(String[] args) {
 
-//        Demo01_Arithmetic.ex_01();
+//        Demo01_Arithmetic.basicMethod();
         Demo01_Arithmetic.compoundAssignmentOperator();
+//        Demo01_Arithmetic.incrementDecrementOperator();
+
 
     }
 
--- src/Lesson_01/Demo01_Arithmetic.java
+++ src/Lesson_01/Demo01_Arithmetic.java
@@ -4,84 +4,7 @@ import java.util.Scanner;
 
 public class Demo01_Arithmetic {
 
-    public static void main(String[] args) {
-
-        boolean isDrivable = true;
-
-        char ch = 'A';
-
-        byte byteVal;
-
-        short shortVal;
-
-        int intValue;
-
-        long longNumber;
-
-        float floatNumber;
-
-        double doubleNumber;
-
-        // Arithmetic
-
-        int a = 23;
-
-        // Addition
-        System.out.println("Addition");
-        System.out.println("a + 10 = " + (a + 10) );
-        System.out.println("----------");
-
-        // Subtraction
-        System.out.println("Subtraction");
-        System.out.println("a - 10 = " + (a - 10) );
-        System.out.println("----------");
-
-        // Multiplication
-        System.out.println("Multiplication");
-        System.out.println("a * 10 = " + (a * 10) );
-        System.out.println("----------");
-
-        // Division
-        System.out.println("Division");
-        System.out.println("a / 10 = " + (a / 10) );
-        System.out.println("----------");
-
-        // Remainder (mod)
-        System.out.println("Remainder (mod)");
-        System.out.println("a % 10 = " + (a % 10) );
-        System.out.println("----------");
-
-        // ------------------ Compound Assigment Operators --------------------
-
-        int b = 0;
-
-        b = b + 25;
-        System.out.println("b + 25 -> " + b );
-
-        b += 25;
-        System.out.println("b += 25 -> " + b );
-        System.out.println("----------");
-
-        int c = 101;
-        System.out.println("101 % 25 -> " + (101 % 25));
-
-        c %= 25;
-        System.out.println("c %= 25 -> " + c);
-        System.out.println("----------");
-
-        // ------------------ Increment / Decrement Operators --------------------
-
-        int m = 1, n = 3, o = 5;
-
-        int x = m++ + n++ + ++o;    // 1 + 3 + 6
-        System.out.println("x = " + x);
-
-        x = m+=3 + ++n - o++;       // 5 + 5 - 6
-        System.out.println("x = " + x);
-
-    }
-
-    public static void ex_01(){
+    public static void basicMethod(){
         System.out.println("--- Basic Operator ---");
         System.out.printf("Please Input A : ");
         Scanner keyboard = new Scanner(System.in);
@@ -184,6 +107,7 @@ public class Demo01_Arithmetic {
         Scanner keyboard = new Scanner(System.in);
         int a = keyboard.nextInt();
         int final_num = (((a+5-10)*2)/10)%2;
+
         a = compound_add_method(a);
         a = compound_minus_method(a);
         a = compound_multiple_method(a);
@@ -264,4 +188,7 @@ public class Demo01_Arithmetic {
         return a;
     }
 
+    public static void incrementDecrementOperator(){
+
+    }
 }
